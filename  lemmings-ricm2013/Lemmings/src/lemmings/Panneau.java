6cef0baf0bc0b947d9b4c81648531e836ba7279e
jerome.barbier26 <jerome.barbier26@3a2bb292-2e5d-63f8-9446-9263169b18c7>
2013-06-18 07:34:30 +0000

    rajout du menu pour que l'utilisateur charge des fichiers

---  lemmings-ricm2013/Lemmings/src/lemmings/Panneau.java
+++  lemmings-ricm2013/Lemmings/src/lemmings/Panneau.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Panneau extends JPanel implements Constantes{ 
