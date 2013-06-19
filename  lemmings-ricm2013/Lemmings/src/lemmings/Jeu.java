f736f376f7d92a90dd16c5dc21c3aabaa70f1a92
david.levayer@gmail.com <david.levayer@gmail.com@3a2bb292-2e5d-63f8-9446-9263169b18c7>
2013-06-19 10:15:39 +0000

    Trololololl (java bricolage)

---  lemmings-ricm2013/Lemmings/src/lemmings/Jeu.java
+++  lemmings-ricm2013/Lemmings/src/lemmings/Jeu.java
@@ -10,55 +10,32 @@ import java.util.List;
 public class Jeu implements Constantes {
 
 	public static List<Automate>listeAutomates = new ArrayList<Automate>();
-
+	
+	public static Fenetre f;
 	public static void main(String[] args) throws InterruptedException, IOException{
 
 
 
 
 		Carte.initCmp(0, 0, 0, 0, 0, 0, 0, 0);
-		Jeu.initialiserJeu("Images/Carte2.png", "Images/chat.png", "", "Documents partagés/automate.xml", new Point(60,55), new Point(330,125), 20);
+		Jeu.initialiserJeu("Images/Carte2.png", 
+						   "Images/chat.png",
+						   "", "Documents partagÃ©s/automate.xml",
+						   new Point(60,55), 
+						   new Point(330,125), 
+						   20);
 		
 		int wait = delaiPop + 1;
 
-		Fenetre f = new Fenetre(tailleFenetreH,tailleFenetreV);
+		f = new Fenetre(tailleFenetreH,tailleFenetreV);
 		f.afficher();
 
-		// Lemmings bloqueurs
-		//Carte.obs.add(new Lemming(100,61,lemmingStop));
-		//Carte.obs.add(new Lemming(80,80,lemmingStop));
-		//Carte.obs.add(new Lemming(80,80,lemmingStop));
-		//Carte.obs.add(new Lemming(160,30,lemmingStop));
-
 		f.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-
-
-		// Lemmings creuseurs
-		//Carte.obs.add(new Lemming(80,80,lemmingCreuseur));
-		//Carte.obs.add(new Lemming(330,80,lemmingCreuseur));
-
-
-		// Lemmings catapultes
-
-
-		//Carte.obs.add(new Lemming(250,150,lemmingCatapulte));
-		//Carte.obs.add(new Lemming(350,150,lemmingCatapulte));
-		//Carte.obs.add(new Lemming(250,150,lemmingCatapulte));
-		//Carte.obs.add(new Lemming(350,150,lemmingCatapulte));
-		//((Lemming) Carte.obs.get(0)).setDirection(gauche);
-		//((Lemming) Carte.obs.get(1)).setDirection(gauche);*/
-
-
-
+		
 		Carte.obs.add(new Lemming(400,150,Math.PI/4,50));
-
-
-
-
-
-
+		
 		while(true) {
-			//f.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+			
 			if(wait>delaiPop && Carte.getNbLemmings()!=0) {
 				Carte.popLemmings(lemmingBase);
 				wait = 0;
@@ -84,17 +61,19 @@ public class Jeu implements Constantes {
 		listeAutomates.add(new AutoLemmingStop(5));
 		listeAutomates.add(new AutoLemmingCatapulte(5));
 
-		Carte.miniMap = "Images/Carte.png";
 		//Thread playWave=new AePlayWave(musique);
 		//playWave.start();
-
-
+		
 		Carte.charger(map,background);
 		
 		Carte.setEntree(ptEntree);
 		Carte.setSortie(ptSortie);
 
 		Carte.setNbLemmings(nbLemmings);
+		if (f!=null) {
+			f.setVisible(false);
+			f = new Fenetre(tailleFenetreH,tailleFenetreV);
+		}
 
 	}
 
