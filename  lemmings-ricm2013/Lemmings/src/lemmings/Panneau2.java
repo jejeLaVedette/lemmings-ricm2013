2958df0e508de9a8266ca38c853e66368e98570d
david.levayer@gmail.com <david.levayer@gmail.com@3a2bb292-2e5d-63f8-9446-9263169b18c7>
2013-06-20 09:17:26 +0000

    rectangle sur la minimap

---  lemmings-ricm2013/Lemmings/src/lemmings/Panneau2.java
+++  lemmings-ricm2013/Lemmings/src/lemmings/Panneau2.java
@@ -2,7 +2,10 @@ package lemmings;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+
 import javax.swing.JPanel;
+import javax.swing.JViewport;
 
 public class Panneau2 extends JPanel implements Constantes { 
 	/**
@@ -17,7 +20,7 @@ public class Panneau2 extends JPanel implements Constantes {
 	private int tailleRY;
 	private double coefFenetre = 0.75;
 	public static final int coeff = 10;
-	
+
 	private int coefRetrecissementx;
 	private int coefRetrecissementy;
 
@@ -25,13 +28,13 @@ public class Panneau2 extends JPanel implements Constantes {
 		//on test les x
 		/*if(tailleFX >= Carte.LARGEUR_CARTE) this.tailleRX = Fenetre.tailleFX/Carte.LARGEUR_CARTE;
 		else this.tailleRX = Carte.LARGEUR_CARTE/Fenetre.tailleFX;
-		
+
 		//on test les y
 		if(coefFenetre*tailleFY>=Carte.HAUTEUR_CARTE) this.tailleRY = (int)(coefFenetre*Fenetre.tailleFY/Carte.HAUTEUR_CARTE);
 		else this.tailleRY = (int) (Carte.HAUTEUR_CARTE/coefFenetre*Fenetre.tailleFY);
-		
+
 		this.coefRetrecissement = 3;*/
-		
+
 		if(tailleFX >= Carte.LARGEUR_CARTE) {
 			this.tailleRX = Fenetre.tailleFX/Carte.LARGEUR_CARTE;
 			this.coefRetrecissementx = this.tailleRX;
@@ -40,7 +43,7 @@ public class Panneau2 extends JPanel implements Constantes {
 			this.tailleRX = 1;
 			this.coefRetrecissementx = this.tailleRX*2+1;//*2 car la mini_map n'est que 1/2 de x de l'�cran et +1 pour prendre le pixel sup�rieur
 		}
-		
+
 		//on test les y
 		if(coefFenetre*tailleFY>=Carte.HAUTEUR_CARTE) {
 			this.tailleRY = (int)(coefFenetre*Fenetre.tailleFY/Carte.HAUTEUR_CARTE);
@@ -50,22 +53,26 @@ public class Panneau2 extends JPanel implements Constantes {
 			this.tailleRY =1;
 			this.coefRetrecissementy = this.tailleRY*2+1;
 		}
-		
+
 	}
 
 	public void paintComponent(Graphics g)
 	{
 		int luminance; Color c;
-		System.out.println(Fenetre.scroll.getViewport().getY());
+
 		for(int i=0; i<Carte.HAUTEUR_CARTE; i+=coefRetrecissementy){
 
 			for(int j=0; j<Carte.LARGEUR_CARTE; j+=coefRetrecissementx){ 
 				
-				if (j == Fenetre.scroll.getViewport().getViewPosition().y || 
-					j == Fenetre.scroll.getViewport().getViewPosition().y + Fenetre.scroll.getBounds().width -1 )
-				{
-					g.setColor(new Color(0,255,0));
-				}
+				// Si on est sur le rectangle qui représente le cadre visible de la map
+				Rectangle view = Fenetre.scroll.getViewport().getViewRect();
+				
+				if (tailleRX==1 && Math.abs(j-view.x)<5 && i>(view.y) && i<(view.y+view.height)||
+					tailleRX==1 && Math.abs(j-view.x-view.width)<5 && i>(view.y) && i<(view.y+view.height) ||
+					tailleRY==1 && Math.abs(i-view.y)<3 && j>(view.x) && j<(view.x+view.width) ||
+					tailleRY==1 && Math.abs(i-view.y-view.height)<3 && j>(view.x) && j>(view.x+view.width))
+					g.setColor(new Color(0,255,0));					
+				
 				// Si c'est du sol, on affiche en nuance de gris
 				else if (Carte.map[j][i].isSol())
 				{
@@ -76,12 +83,12 @@ public class Panneau2 extends JPanel implements Constantes {
 				// Si c'est de l'air, on affiche en gris clair
 				else if (Carte.map[j][i].isAir())
 					g.setColor(new Color(200,200,200));
-				
+
 				g.fillRect(posX, posY, tailleRX, tailleRY);
 
 				// on avance en longueur
 				posX=posX+tailleRX;
-				
+
 			}
 			//on se replace tout a gauche de l'écran
 			posX=0;
@@ -93,16 +100,16 @@ public class Panneau2 extends JPanel implements Constantes {
 		//si on repaint, on repart de 0
 		posX=0;
 		posY=0;
-		
+
 		//on affiche les lemmings
 		for(int k =0;k<Carte.obs.size();k++){				
 			g.setColor(new Color(0,200,0));
 			g.fillRect(Carte.obs.get(k).getX()*tailleRX/coefRetrecissementx-tailleRX*coeff/2, 
-					   (Carte.obs.get(k).getY()-coeff)*tailleRY/coefRetrecissementy, 
-					   Constantes.coeff/2, 
-					   Constantes.coeff);
+					(Carte.obs.get(k).getY()-coeff)*tailleRY/coefRetrecissementy, 
+					Constantes.coeff/2, 
+					Constantes.coeff);
 		}
-		
+
 
 	}
 
